3b8d1f87566477947c07fbac10004e049df20f97
Guilherme Henrique Nascimento <gnascimento@gmail.com>
2019-08-27 16:47:27 -0300

    Removido Pastas

--- js/init.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-  $(function() {
-
-    $('.sidenav').sidenav();
-    $('.materialboxed').materialbox();
-    M.AutoInit();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
