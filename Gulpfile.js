abe1ad5cfa1fec7b82766f8242ba7675a2f23c5c
Guilherme Henrique PC Casa <guilhermen@alunos.utfpr.edu.br>
2019-08-26 23:38:39 -0300

    Foi Trabalhado na interface do sistema, Criado uma pagina de cadastro e reformulado a panina de login

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,29 +3,32 @@ var sass = require('gulp-sass');
 var clean = require('gulp-clean');
 
 
-gulp.task('sass',(done)=> {
-   gulp.src('./dev/scss/*.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./assets/css'));
-   done();
-   console.log("done scss!")
+gulp.task('sass', (done) => {
+  gulp.src('./dev/scss/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+  done();
+  console.log("done scss!")
 });
 
 
 gulp.task('clear', (done) => {
-  return gulp.src('./assets/css/style.css', {read: false})
-  .pipe(clean());
+  return gulp.src('./assets/css/style.css', {
+      read: false
+    })
+    .pipe(clean());
   done();
   console.log('done clear!');
 });
 
 gulp.task('default',
 
-gulp.series('clear','sass')
-,
+  gulp.series('clear', 'sass'),
 
-(done) => {
-  console.log("teste!!!")
-  done()
-  console.log("done!")
-});
+  (done) => {
+    console.log("teste!!!")
+    done()
+    console.log("done!")
+  });
