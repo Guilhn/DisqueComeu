d63b6f09b6cd7303e2a9386c921f8b2b67353c59
Guilherme Henrique Nascimento <gnascimento@gmail.com>
2019-08-29 17:09:00 -0300

    Foi feito toda a navegação do sistema, corrigido a cor focus da parte login e criado as paginas de adm e consumidor

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,9 +3,9 @@ var a = "admin";
 $(document).ready(function() {
   $('#logar').on('click', function() {
     if (a == $('#username').val()) {
-      window.location.href = "../pages/template/administrador.html";
+      window.location.href = "../pages/template/administrador/administrador.html";
     } else {
-      window.location.href = "../pages/template/consumidor.html";
+      window.location.href = "../pages/template/consumidor/consumidor.html";
     }
   });
 
