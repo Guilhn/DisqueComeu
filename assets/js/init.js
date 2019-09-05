3ad839721d40da6521c074d8b0ed0fc20b5561c6
Guilherme Henrique Nascimento <gnascimento@gmail.com>
2019-09-05 17:08:10 -0300

    Foi criado a tela de cadastrar e criar pratos

--- assets/js/init.js
+++ assets/js/init.js
@@ -6,6 +6,7 @@
     $(".dropdown-trigger").dropdown();
     $('.tooltipped').tooltip();
     $('.modal').modal();
+    $('select').formSelect();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
